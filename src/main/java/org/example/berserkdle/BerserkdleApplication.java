1d7b9787aa4fe42d35d3b53eaafe06200c25d947
Tema <sezemov2@gmail.com>
2025-11-07 23:42:45 +0300

    init commit

---
+++ src/main/java/org/example/berserkdle/BerserkdleApplication.java
@@ -0,0 +1,13 @@
+package org.example.berserkdle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerserkdleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BerserkdleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/berserkdle/BerserkdleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.berserkdle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerserkdleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
