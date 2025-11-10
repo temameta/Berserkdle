6ece74d1006b4e66a51c66699923e2eca2d27bf9
Tema <sezemov2@gmail.com>
2025-11-10 13:23:37 +0300

    added DTO for person

--- src/main/java/org/example/berserkdle/dtos/PersonDTO.java
+++ src/main/java/org/example/berserkdle/dtos/PersonDTO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -15,6 +16,16 @@ public class PersonDTO {
     private String firstArc;
     private String gender;
     private String species;
-    private List<String> groups;
-    private List<String> weapons;
+    private List<String> groups = new ArrayList<>();
+    private List<String> weapons = new ArrayList<>();
+
+    public void addGroup(String groupName) {
+        if (!this.groups.contains(groupName))
+            this.groups.add(groupName);
+    }
+
+    public void addWeapon(String weaponName) {
+        if (!this.weapons.contains(weaponName))
+            this.weapons.add(weaponName);
+    }
 }
