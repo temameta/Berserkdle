b20f1e452637620828243881fb45496a57c91b81
Tema <sezemov2@gmail.com>
2025-11-10 09:46:46 +0300

    added PersonDTO.java

--- src/main/java/org/example/berserkdle/dtos/PersonDTO.java
+++ src/main/java/org/example/berserkdle/dtos/PersonDTO.java
@@ -1,5 +1,20 @@
 package org.example.berserkdle.dtos;
 
-public class PersonDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PersonDTO {
+    private Long id;
+    private String name;
+    private String firstArc;
+    private String gender;
+    private String species;
+    private List<String> groups;
+    private List<String> weapons;
 }
