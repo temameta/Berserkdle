e4525b83f80d9bd89b00a4cfe0b831cb03aa27f9
Tema <sezemov2@gmail.com>
2025-11-10 19:11:24 +0300

    added DTOs

---
+++ src/main/java/org/example/berserkdle/dtos/GroupDTO.java
@@ -0,0 +1,13 @@
+package org.example.berserkdle.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroupDTO {
+    private Long id;
+    private String name;
+}
