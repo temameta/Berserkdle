f0326192a9c667ea00ad30bf80831787b6017ee3
Tema <sezemov2@gmail.com>
2025-11-10 09:49:37 +0300

    added service for person

---
+++ src/main/java/org/example/berserkdle/services/PersonService.java
@@ -0,0 +1,14 @@
+package org.example.berserkdle.services;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PersonService {
+
+}
