039c1772a03a2d93ba70001878160acd0dff4cc2
Tema <sezemov2@gmail.com>
2025-11-18 22:01:49 +0300

    updated InterfaceController

--- src/main/java/org/example/berserkdle/controllers/mvc/moderator/InterfaceController.java
+++ src/main/java/org/example/berserkdle/controllers/mvc/moderator/InterfaceController.java
@@ -1,12 +1,13 @@
 package org.example.berserkdle.controllers.mvc.moderator;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public interface InterfaceController {
     @GetMapping("/get")
     String get();
     @GetMapping("/create")
-    String create();
+    String create(Model model);
     @GetMapping("/update")
     String update();
     @GetMapping("/delete")
