0db9d4b62ada47bc4ee25d813971e2c6e78aaf29
Tema <sezemov2@gmail.com>
2025-11-18 13:41:17 +0300

    updated moderator controller (all fixed now)

--- src/main/java/org/example/berserkdle/controllers/mvc/moderator/ModeratorController.java
+++ src/main/java/org/example/berserkdle/controllers/mvc/moderator/ModeratorController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping()
+@RequestMapping("/moderator")
 public class ModeratorController {
-    @GetMapping("/home")
+    @GetMapping()
     public String home() {
         return "moderator";
     }
