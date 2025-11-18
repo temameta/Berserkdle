692955c4297b6c897f2d013d430da816127859ec
Tema <sezemov2@gmail.com>
2025-11-18 22:03:32 +0300

    added method for creation page and path

--- src/main/java/org/example/berserkdle/controllers/mvc/moderator/WeaponController.java
+++ src/main/java/org/example/berserkdle/controllers/mvc/moderator/WeaponController.java
@@ -1,10 +1,12 @@
 package org.example.berserkdle.controllers.mvc.moderator;
 
+import org.example.berserkdle.dtos.WeaponDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/moderator/weapon")
+@RequestMapping("/weapon")
 public class WeaponController implements InterfaceController {
     @Override
     public String get() {
@@ -12,8 +14,10 @@ public class WeaponController implements InterfaceController {
     }
 
     @Override
-    public String create() {
-        return "";
+    public String create(Model model) {
+        WeaponDTO weaponDTO = new WeaponDTO();
+        model.addAttribute("weapon", weaponDTO);
+        return "weapon/creation";
     }
 
     @Override
