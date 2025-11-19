d8639f8cf42b84cb25387544eea0cb3013a34578
Tema <sezemov2@gmail.com>
2025-11-19 18:31:22 +0300

    added method of creation

--- src/main/java/org/example/berserkdle/controllers/mvc/moderator/InterfaceController.java
+++ src/main/java/org/example/berserkdle/controllers/mvc/moderator/InterfaceController.java
@@ -1,13 +1,18 @@
 package org.example.berserkdle.controllers.mvc.moderator;
 
+import org.example.berserkdle.dtos.AbstractDTO;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-public interface InterfaceController {
+public interface InterfaceController<D extends AbstractDTO> {
     @GetMapping("/get")
     String get();
     @GetMapping("/create")
     String create(Model model);
+    @PostMapping("/create")
+    String create(D DTO, RedirectAttributes redirectAttributes);
     @GetMapping("/update")
     String update();
     @GetMapping("/delete")
--- src/main/java/org/example/berserkdle/controllers/mvc/moderator/WeaponController.java
+++ src/main/java/org/example/berserkdle/controllers/mvc/moderator/WeaponController.java
@@ -1,13 +1,22 @@
 package org.example.berserkdle.controllers.mvc.moderator;
 
 import org.example.berserkdle.dtos.WeaponDTO;
+import org.example.berserkdle.services.WeaponService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/weapon")
-public class WeaponController implements InterfaceController {
+public class WeaponController implements InterfaceController<WeaponDTO> {
+    private final WeaponService weaponService;
+
+    public WeaponController(WeaponService weaponService) {
+        this.weaponService = weaponService;
+    }
+
     @Override
     public String get() {
         return "";
@@ -17,6 +26,13 @@ public class WeaponController implements InterfaceController {
     public String create(Model model) {
         model.addAttribute("weapon", new WeaponDTO());
         return "weapon/creation";
+    }
+
+    @Override
+    public String create(WeaponDTO weaponDTO, RedirectAttributes redirectAttributes) {
+        weaponService.save(weaponDTO);
+        redirectAttributes.addFlashAttribute("successMessage", "Оружие " + weaponDTO.getName() + " успешно добавлено!");
+        return "redirect:/weapon/create";
     }
 
     @Override
