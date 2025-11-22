9a1efd726bba2a43fca1c811af01535090e12450
Tema <sezemov2@gmail.com>
2025-11-22 20:01:10 +0300

    added validation

--- src/main/java/org/example/berserkdle/controllers/mvc/moderator/InterfaceController.java
+++ src/main/java/org/example/berserkdle/controllers/mvc/moderator/InterfaceController.java
@@ -2,6 +2,7 @@ package org.example.berserkdle.controllers.mvc.moderator;
 
 import org.example.berserkdle.dtos.AbstractDTO;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -12,7 +13,7 @@ public interface InterfaceController<D extends AbstractDTO> {
     @GetMapping("/create")
     String create(Model model);
     @PostMapping("/create")
-    String create(D DTO, RedirectAttributes redirectAttributes);
+    String create(D DTO, BindingResult bindingResult, RedirectAttributes redirectAttributes);
     @GetMapping("/update")
     String update();
     @GetMapping("/delete")
--- src/main/java/org/example/berserkdle/controllers/mvc/moderator/WeaponController.java
+++ src/main/java/org/example/berserkdle/controllers/mvc/moderator/WeaponController.java
@@ -4,6 +4,7 @@ import org.example.berserkdle.dtos.WeaponDTO;
 import org.example.berserkdle.services.WeaponService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -29,7 +30,13 @@ public class WeaponController implements InterfaceController<WeaponDTO> {
     }
 
     @Override
-    public String create(WeaponDTO weaponDTO, RedirectAttributes redirectAttributes) {
+    public String create(WeaponDTO weaponDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            //log.warn("Ошибки валидации при добавлении компании: {}", bindingResult.getAllErrors());
+            redirectAttributes.addFlashAttribute("weapon", weaponDTO);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.weapon", bindingResult);
+            return "redirect:/weapon/create";
+        }
         weaponService.save(weaponDTO);
         redirectAttributes.addFlashAttribute("successMessage", "Оружие " + weaponDTO.getName() + " успешно добавлено!");
         return "redirect:/weapon/create";
