f87328109f23d6d7f814dd8ae8491a7d07be6136
Tema <sezemov2@gmail.com>
2025-11-24 23:24:33 +0300

    deleted unused imports

--- src/main/java/org/example/berserkdle/dtos/AbstractDTO.java
+++ src/main/java/org/example/berserkdle/dtos/AbstractDTO.java
@@ -1,14 +1,9 @@
 package org.example.berserkdle.dtos;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-import org.example.berserkdle.utils.validation.UniqueName;
-import org.hibernate.validator.constraints.UniqueElements;
 
 @Data
 @AllArgsConstructor
