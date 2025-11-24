b90a7159c522517e4e80f53910f1f9b36c323572
Tema <sezemov2@gmail.com>
2025-11-24 15:43:15 +0300

    removed implementation of interface because of annotations of mapping are not inherit

--- src/main/java/org/example/berserkdle/controllers/mvc/moderator/WeaponController.java
+++ src/main/java/org/example/berserkdle/controllers/mvc/moderator/WeaponController.java
@@ -3,18 +3,22 @@ package org.example.berserkdle.controllers.mvc.moderator;
 import jakarta.validation.Valid;
 import org.example.berserkdle.dtos.WeaponDTO;
 import org.example.berserkdle.services.WeaponService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/weapon")
-public class WeaponController implements InterfaceController<WeaponDTO> {
+public class WeaponController {
     private final WeaponService weaponService;
 
+    @Autowired
     public WeaponController(WeaponService weaponService) {
         this.weaponService = weaponService;
     }
@@ -24,17 +28,17 @@ public class WeaponController implements InterfaceController<WeaponDTO> {
         return new WeaponDTO();
     }
 
-    @Override
+    @GetMapping("/get")
     public String get() {
         return "";
     }
 
-    @Override
+    @GetMapping("/create")
     public String create() {
         return "weapon/create";
     }
 
-    @Override
+    @PostMapping("/create")
     public String create(@Valid WeaponDTO weaponModel, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
         if (bindingResult.hasErrors()) {
             //log.warn("Ошибки валидации при добавлении компании: {}", bindingResult.getAllErrors());
@@ -47,12 +51,12 @@ public class WeaponController implements InterfaceController<WeaponDTO> {
         return "redirect:/weapon/create";
     }
 
-    @Override
+    @GetMapping("/update")
     public String update() {
         return "";
     }
 
-    @Override
+    @GetMapping("/delete")
     public String delete() {
         return "";
     }
